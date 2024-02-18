ff6f8b30a713cb4f021c30b3a6d66a94d5404c78
Bishal Shah <great.shah222@gmail.com>
2024-02-18 23:39:36 +0200

    chat component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 import { ChatHeader } from "@/components/chat/chat-header";
 import { currentProfile } from "@/lib/current-profile";
+import { ChatInput } from "@/components/chat/chat-input";
 import { db } from "@/lib/db";
 
 interface ChannelIdPageProps {
@@ -41,6 +42,18 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 	return (
 		<div className="bg-white dark:bg-[#313338] flex flex-col h-full">
 			<ChatHeader serverId={serverId} name={channel?.name} type="channel" />
+
+			<div className="flex-1">Future message</div>
+
+			<ChatInput
+				name={channel.name}
+				type="channel"
+				apiUrl="/api/socket/messages"
+				query={{
+					channelId: channel.id,
+					serverId: channel.serverId,
+				}}
+			/>
 		</div>
 	);
 };
